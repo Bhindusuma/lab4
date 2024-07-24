d4eef1acda9c1733bfa4af353a4b755267cbda17
Bhindusuma <160280238+Bhindusuma@users.noreply.github.com>
2024-07-23 23:41:51 -0400

    app.py

--- app.py
+++ app.py
@@ -23,6 +23,6 @@ def predict():
     return jsonify({'weight': prediction[0]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
     app.run(debug=True)
 
